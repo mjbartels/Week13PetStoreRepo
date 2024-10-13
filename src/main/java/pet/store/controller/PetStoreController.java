b8aebb134894530a69fc8cce34f5e761909b7a4c
Matthew Bartels <matthewbartels@Macintosh.attlocal.net>
2024-10-13 12:34:00 -0400

    Week 15 coding commit for pet store app

--- src/main/java/pet/store/controller/PetStoreController.java
+++ src/main/java/pet/store/controller/PetStoreController.java
@@ -1,9 +1,14 @@
 package pet.store.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -12,6 +17,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import lombok.extern.slf4j.Slf4j;
 import pet.store.service.PetStoreService;
 import pet.store.controller.model.PetStoreData;
+import pet.store.controller.model.PetStoreData.PetStoreCustomer;
+import pet.store.controller.model.PetStoreData.PetStoreEmployee;
 
 @RestController
 @RequestMapping("/pet_store")
@@ -36,5 +43,43 @@ public class PetStoreController {
     	log.info("Updating pet store {}", petStoreData);
     		return petStoreService.savePetStore(petStoreData);
     		
+    }
+    
+    @PostMapping("/{petStoreId}/employee")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public PetStoreEmployee addEmployeeToPetStore(@PathVariable Long petStoreId, 
+    		@RequestBody PetStoreEmployee petStoreEmployee) {
+    	log.info("Adding employee {} to pet store with ID=()", petStoreEmployee, petStoreId);
+    	
+    	return petStoreService.saveEmployee(petStoreId, petStoreEmployee);
+    }
+    
+    @PostMapping("/{petStoreId}/customer")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public PetStoreCustomer addCustomerToPetStore(@PathVariable Long petStoreId,
+    		@RequestBody PetStoreCustomer petStoreCustomer) {
+    	log.info("Adding customer {} to pet store ID={}", petStoreCustomer,
+    			petStoreId);
+    	
+    	return petStoreService.saveCustomer(petStoreId, petStoreCustomer);
+    }
+    
+    @GetMapping
+    public List<PetStoreData> retrieveAllPetStores() {
+    	log.info("Retrieving all pet stores");
+    	return petStoreService.retrieveAllPetStores();
+    }
+    
+    @GetMapping("/{petStoreId}")
+    public PetStoreData retrievePetStoreById(@PathVariable Long petStoreId) {
+    	log.info("Retrieving pet store with ID={}", petStoreId);
+    	return petStoreService.retrievePetStoreById(petStoreId);
+    }
+    
+    @DeleteMapping("/{petStoreId}")
+    public Map<String, String> deletePetstoreById(@PathVariable Long petStoreId) {
+       log.info("Deleting pet store with ID={}", petStoreId);
+       petStoreService.deletePetStoreById(petStoreId);
+       return Map.of("message", "Pet store with ID=" + petStoreId + " has been deleted.");
     }
 }
